4642908a2394f9121f5b9b51c14045d68be8d517
Takahy <tk4331nakano@gmail.com>
2020-10-27 15:58:01 +0900

    first commit

---
+++ tests/test_views.py
@@ -0,0 +1,13 @@
+from aptest.views.default import my_view
+from aptest.views.notfound import notfound_view
+
+
+def test_my_view(app_request):
+    info = my_view(app_request)
+    assert app_request.response.status_int == 200
+    assert info['project'] == 'aptest'
+
+def test_notfound_view(app_request):
+    info = notfound_view(app_request)
+    assert app_request.response.status_int == 404
+    assert info == {}
